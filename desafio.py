eac08ce228e48906abfa637933565776d6073a33
Rennan Santos <rennan-ssantos@hotmail.com>
2023-07-06 18:27:45 -0300

    Commit substring palindromo
    código feito referente a substring palindromo

--- desafio.py
+++ desafio.py
@@ -16,3 +16,21 @@ def remover(string):
 entrada_remover = "Hello, World!"
 saida_remover = remover(entrada_remover)
 print("Remover : ",saida_remover)
+
+# 3 - Encontrar a substring palindrômica mais longa na string
+def substring(string):
+    max_length = 0
+    maior_substring = ""
+
+    for i in range(len(string)):
+        for j in range(i+1, len(string)+1):
+            substring = string[i:j]
+            if substring == substring[::-1] and len(substring) > max_length:
+                max_length = len(substring)
+                maior_substring = substring 
+    
+    return maior_substring
+
+entrada_substring = "String Palindromo definição: Uma palavra é dita palíndroma quando possui o mesmo significado se lida da esquerda para a direita ou da direita para a esquerda"
+saida_substring = substring(entrada_substring)
+print("Substring: ", saida_substring)
